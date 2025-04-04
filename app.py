8272f71da49e5082a08e9451d316ed95a98533be
Rizal Widyananda <43237337+rizalw@users.noreply.github.com>
2025-04-04 22:15:06 +0700

    Prepare for first deploy

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def model_train_process():
     y = df[dataset.label_name]
 
     from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
     
     #Train Model
     trained_model = new_model.fit(X_train, y_train)
