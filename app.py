0d401435579267b9bc5a1b0cd254012619f71daa
Rizal Widyananda <43237337+rizalw@users.noreply.github.com>
2025-04-28 22:10:47 +0700

    add some auth countermeasure

--- app.py
+++ app.py
@@ -108,115 +108,126 @@ def model_train_parameter(id):
 @app.post("/dashboard/model/train/process")
 @login_required
 def model_train_process():
-    model_id = request.form["model_id"]
-    dataset_id = request.form["dataset_id"]
-    algorithm_id = int(request.form["algorithm_id"])
-    algorithm_name = request.form["algorithm_name"]
-    possible_parameter = ["categorical", "int", "float", "boolean"] # type: ignore
-    parameters = {}
-    
-    # Input every available parameters 
-    for param in possible_parameter:
-        if param in algorithms[algorithm_id]["parameters"].keys():          
-            for key in algorithms[algorithm_id]["parameters"][param].keys():
-                if param == "categorical":
-                    if request.form[key] == "None":
-                        parameters[key] = None
-                    else:
-                        parameters[key] = request.form[key]
-                elif param == "int":
-                    if request.form[key] == "" or request.form[key] == None:
-                        parameters[key] = None
-                    else:
-                        parameters[key] = int(request.form[key])
-                elif param == "float":
-                    parameters[key] = float(request.form[key])
-                elif param == "boolean":
-                    parameters[key] = bool(request.form[key])
-    
-    #Generate new model
-    from sklearn.svm import LinearSVC
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.neighbors import KNeighborsClassifier
-    
-    if algorithm_id == 1:
-        new_model = DecisionTreeClassifier(**parameters)    
-    elif algorithm_id == 2:
-        new_model = LinearSVC(**parameters) 
-    elif algorithm_id == 3:
-        new_model = KNeighborsClassifier(**parameters)
+    if current_user.role != "customer":
+        return redirect(url_for("auth.logout"))
+    else:
+        model_id = request.form["model_id"]
+        dataset_id = request.form["dataset_id"]
+        algorithm_id = int(request.form["algorithm_id"])
+        algorithm_name = request.form["algorithm_name"]
+        possible_parameter = ["categorical", "int", "float", "boolean"] # type: ignore
+        parameters = {}
+        
+        # Input every available parameters 
+        for param in possible_parameter:
+            if param in algorithms[algorithm_id]["parameters"].keys():          
+                for key in algorithms[algorithm_id]["parameters"][param].keys():
+                    if param == "categorical":
+                        if request.form[key] == "None":
+                            parameters[key] = None
+                        else:
+                            parameters[key] = request.form[key]
+                    elif param == "int":
+                        if request.form[key] == "" or request.form[key] == None:
+                            parameters[key] = None
+                        else:
+                            parameters[key] = int(request.form[key])
+                    elif param == "float":
+                        parameters[key] = float(request.form[key])
+                    elif param == "boolean":
+                        parameters[key] = bool(request.form[key])
+        
+        #Generate new model
+        from sklearn.svm import LinearSVC
+        from sklearn.tree import DecisionTreeClassifier
+        from sklearn.neighbors import KNeighborsClassifier
+        
+        if algorithm_id == 1:
+            new_model = DecisionTreeClassifier(**parameters)    
+        elif algorithm_id == 2:
+            new_model = LinearSVC(**parameters) 
+        elif algorithm_id == 3:
+            new_model = KNeighborsClassifier(**parameters)
 
-    #Get csv file
-    dataset = db.one_or_404(db.select(Dataset).filter_by(id = dataset_id))
-    df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'] , dataset.filepath))
+        #Get csv file
+        dataset = db.one_or_404(db.select(Dataset).filter_by(id = dataset_id))
+        df = pd.read_csv(os.path.join(app.config['UPLOAD_FOLDER'] , dataset.filepath))
 
-    #Splitting data
-    X = df.drop(columns=[dataset.label_name])
-    y = df[dataset.label_name]
+        #Splitting data
+        X = df.drop(columns=[dataset.label_name])
+        y = df[dataset.label_name]
 
-    test_size = float(request.form['test_size'])
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=1)
-    
-    #Train Model
-    trained_model = new_model.fit(X_train, y_train)
-    
-    #Check accuracy
-    score = trained_model.score(X_test, y_test)
-    
-    #Generate model and parameter file
-    pickle_parameter = pickle.dumps(parameters, pickle.HIGHEST_PROTOCOL)
+        test_size = float(request.form['test_size'])
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=1)
+        
+        #Train Model
+        trained_model = new_model.fit(X_train, y_train)
+        
+        #Check accuracy
+        score = trained_model.score(X_test, y_test)
+        
+        #Generate model and parameter file
+        pickle_parameter = pickle.dumps(parameters, pickle.HIGHEST_PROTOCOL)
 
-    filename = "{}_{}_{}".format(model_id, dataset_id, algorithm_name)
-    model_path = os.path.join(app.config['UPLOAD_FOLDER'], "model/")
-    if not os.path.exists(model_path):
-        os.mkdir(model_path)
-    filepath = os.path.join(model_path, filename)
-    with open(filepath, "wb") as f:
-        pickle.dump(trained_model, f)
-    
-    #Save the data to DB
-    model = db.one_or_404(db.select(Model).filter_by(id = model_id))
-    model.status = True
-    model.accuracy = score
-    model.parameter = pickle_parameter
-    model.filepath = os.path.join("model/", filename)
-    db.session.commit()
-    
-    return redirect(url_for("model"))
+        filename = "{}_{}_{}".format(model_id, dataset_id, algorithm_name)
+        model_path = os.path.join(app.config['UPLOAD_FOLDER'], "model/")
+        if not os.path.exists(model_path):
+            os.mkdir(model_path)
+        filepath = os.path.join(model_path, filename)
+        with open(filepath, "wb") as f:
+            pickle.dump(trained_model, f)
+        
+        #Save the data to DB
+        model = db.one_or_404(db.select(Model).filter_by(id = model_id))
+        model.status = True
+        model.accuracy = score
+        model.parameter = pickle_parameter
+        model.filepath = os.path.join("model/", filename)
+        db.session.commit()
+        
+        return redirect(url_for("model"))
 
 @app.route("/dashboard/model/predict/<int:id>", methods=['GET', 'POST'])
+@login_required
 def model_predict(id):
-    model = db.one_or_404(db.select(Model).filter_by(id = id))
-    if request.method == "POST":
-        pickle_file = os.path.join(app.config['UPLOAD_FOLDER'] , model.filepath)    
-        
-        with open(pickle_file, 'rb') as picklefile:
-            model_pickle = pickle.load(picklefile)
-        
-        predict_data = {}
-        predict_column = request.form["column_list"].split("-")
-        for column_name in predict_column:
-            predict_data[column_name] = float(request.form[column_name])
-        
-        result = model_pickle.predict([[ x for x in predict_data.values()]])
-        flash("Model : {}".format(model.name))
-        flash("Algorithm : {}".format(algorithms[model.algorithm_id]["name"]))
-        flash("Output : {}".format(result[0]))
-        return redirect(url_for("model"))
+    if current_user.role != "customer":
+        return redirect(url_for("auth.logout"))
     else:
-        label_name = model.dataset.label_name
-        dataset_filepath = os.path.join(app.config['UPLOAD_FOLDER'] , model.dataset.filepath)
-        df = pd.read_csv(dataset_filepath)
-        column_list = df.columns.tolist()
-        column_list.remove(label_name)
-        value_example = df.iloc[0].values.tolist()
-        return render_template("model_predict.html", column_list = column_list, value_example = value_example, id = id)
+        model = db.one_or_404(db.select(Model).filter_by(id = id))
+        if request.method == "POST":
+            pickle_file = os.path.join(app.config['UPLOAD_FOLDER'] , model.filepath)    
+            
+            with open(pickle_file, 'rb') as picklefile:
+                model_pickle = pickle.load(picklefile)
+            
+            predict_data = {}
+            predict_column = request.form["column_list"].split("-")
+            for column_name in predict_column:
+                predict_data[column_name] = float(request.form[column_name])
+            
+            result = model_pickle.predict([[ x for x in predict_data.values()]])
+            flash("Model : {}".format(model.name))
+            flash("Algorithm : {}".format(algorithms[model.algorithm_id]["name"]))
+            flash("Output : {}".format(result[0]))
+            return redirect(url_for("model"))
+        else:
+            label_name = model.dataset.label_name
+            dataset_filepath = os.path.join(app.config['UPLOAD_FOLDER'] , model.dataset.filepath)
+            df = pd.read_csv(dataset_filepath)
+            column_list = df.columns.tolist()
+            column_list.remove(label_name)
+            value_example = df.iloc[0].values.tolist()
+            return render_template("model_predict.html", column_list = column_list, value_example = value_example, id = id)
 
 @app.get("/dashboard/model/download/<int:id>")
+@login_required
 def model_download(id):
-    model = db.one_or_404(db.select(Model).filter_by(id = id))
-    return send_from_directory(app.config["UPLOAD_FOLDER"], model.filepath)
+    if current_user.role != "customer":
+        return redirect(url_for("auth.logout"))
+    else:
+        model = db.one_or_404(db.select(Model).filter_by(id = id))
+        return send_from_directory(app.config["UPLOAD_FOLDER"], model.filepath)
 
 if __name__ == "__main__":
     app.run(debug=True)
